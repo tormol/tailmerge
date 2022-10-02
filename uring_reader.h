231f81a3d5fff5cf98257d9394f70b5ee37b8017
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2022-10-02 16:48:13 +0200

    Document fields of new uring_reader

--- uring_reader.h
+++ uring_reader.h
@@ -15,39 +15,76 @@
 /// Does not deal with splitting into lines or preserving incomplete lines between reads.
 /// (It wants to keep the read pointers aligned.)
 struct uring_reader {
-    // inputs
+    // inputs:
+    /// Number of files to open.
     int files;
+    /// How big the buffer passed to each read should be.
     int per_file_buffer_sz;
     const char* const* filenames;
 
 #ifdef __linux__
-    // ring info && bookkeeping
+    /// Ring info && bookkeeping.
     struct io_uring_params params;
+    /// The file descriptor used to perform syscalls related to the io_uring.
     int ring_fd;
+    /// How many submission entries have been added since io_uring_enter() was last called.
+    /// Increased when adding an entry, passed to io_uring_enter()
+    /// and decremented with how many entries the io_uring_enter() call processed.
     int to_submit;
+    /// The start of the Submission Queue area.
+    /// It is mmap()ed from the ring fd.
     void* sq_ptr;
+    /// The start of the Completion Queue area.
+    /// It is mmap()ed from the ring fd.
     void* cq_ptr;
+    /// The start of the Submission Queue Entries area.
+    /// It is mmap()ed from the ring fd.
+    /// (It is separate from the submission queue because a submission entry is quite big,
+    ///  and some programs might reuse many of the fields.)
     struct io_uring_sqe* sqes;
-    atomic_uint *sring_tail;
-    unsigned int *sring_mask;
-    unsigned int* sring_array;
-    atomic_uint *cring_head;
-    atomic_uint *cring_tail;
-    unsigned int *ring_mask;
-    struct io_uring_cqe* cqes;
-#endif // defined(__linux__)
 
-    // fields for the program logic not solely tied to using the rings
-    char* registered_buffer;
+    // Fields that are a function of the abouve pointers and offsets from .params:
+
+    /// The actual ring buffer of submissions.
+    /// The values are indexes into .sqes.
+    unsigned int* sring_array;
+    /// Incremented when a submission entry is added.
+    /// It doesn't wrap around (until ~0 is reached.
+    atomic_uint *sring_tail;
+    /// To get an index into sring_array:
+    /// .sring_tail & .sring_mask gets an index into .sring_array
+    unsigned int *sring_mask;
+    /// The actual ring buffer of completions.
+    struct io_uring_cqe* cqes;
+    /// Where to start reading completion evets.
+    /// (after anding with .cring_mask)
+    atomic_uint *cring_head;
+    /// Where to stop reading completion evets.
+    /// (after anding with .cring_mask)
+    atomic_uint *cring_tail;
+    unsigned int *cring_mask;
+
+    // Fields for the program logic not solely tied to using the rings:
+
+    /// The uring read operations don't advance the offset for the fd,
+    /// so we have to pass it in and increase it when reads complete.
     off_t* bytes_read;
-    int* lines_read;
-    int* incomplete_line_length;
-    int open_files; // added files - completely read files
-    /// since there is only one buffer per file, and our api is pull-based,
-    /// we need to copy unfinished line to start of buffer when the function is called the next time
-    /// -1 or file index for which buffer[copy_from..copy_from+line_length] should be copied to 0...
-    int needs_copy;
-    int copy_from;
+#endif // defined(__linux__)
+    /// The start of the registered buffer,
+    /// of which each file uses sub-slices.
+    char* registered_buffer;
+    /// For reusing buffers when another file is closed:
+    /// This allows giving the slices to the preceeding open file.
+    /// (This means that the slices of the first file can't be reused:
+    ///  Moving the start of the slices is difficult because an in progress read
+    ///  will fill in from the middle of the new slice.
+    /// (Redistributing buffers fairly would be even more complicated,
+    ///  and would require using vectored reads.)
+    int* buffer_sizes;
+    /// The numer of still open filees:
+    /// .files - completely read files and files we're no longer interested in.
+    /// This is used to detect when there are no more files.
+    int open_files;
 };
 
 /// Initialize struct, create uring and allocate memory
@@ -69,16 +106,18 @@ void uring_destroy(struct uring_reader *r);
 /// Complete setup and submit open & initial read for all files.
 void uring_open_files(struct uring_reader* r, const char* const* filenames, int out_fd);
 
-/// Get the next finished read for a file, but don't wait for it to finish.
-/// Returns {.iov_base=NULL, iov_len=0} if not yet available.
-/// Returns {.iov_base=NULL, iov_len=-1} if end of file.
+/// Get any finished read, submitting queued reads and waiting only if none are ready.
+/// file is set to the file index of the returned read.
+/// Will not return any read for which another read has already been finished.
+/// (This is to avoid callers needing to handle more than one read per file at any time.
 ///
+/// Returns {.iov_base=NULL, iov_len=-1} if no files are open.
 /// If successful, the returned buffer will be marked as "loaned out":
 /// When finished with the read, uring_return_loan() or uring_write_and_return_loan() should be called.
 /// (But they don't need to be called straight away.)
 ///
 /// Intended for "fast forwarding" to a start key, where we want to go through the available bytes.
-struct iovec uring_try_get_next_read(int file_index);
+struct iovec uring_get_any_unloaned(struct uring_reader *r, int *file);
 
 /// Signal that you're done with the read, and that the buffer can be used to read further.
 ///
